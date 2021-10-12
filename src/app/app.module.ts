13d22a9e91d68cf263a09a8de762db47e8812b1c
doublejman76 <42310689+doublejman76@users.noreply.github.com>
2021-10-11 20:33:50 -0500

    changed application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HorrorQueComponent } from './horror-que/horror-que.component';
-import { HomeContainerComponent } from './home-container/home-container.component';
+import { GameConsoleComponent } from './game-console/game-console.component';
+import { GamesComponent } from './games/games.component';
+import { HeaderComponent } from './header/header.component';
+import { PlaygameComponent } from './game-console/playgame/playgame.component';
+import { GamelistComponent } from './games/gamelist/gamelist.component';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeContainerComponent
+    GameConsoleComponent,
+    GamesComponent,
+    HeaderComponent,
+    PlaygameComponent,
+    GamelistComponent,
   ],
   imports: [
     BrowserModule
@@ -21,7 +29,7 @@ export class AppModule {
 
 }
 
-HorrorQueComponent
+
 
 
 
